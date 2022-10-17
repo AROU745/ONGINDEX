a5dee54920e4e1071ecc0c6e4ceda909b72e4f70
YAN <84281944+AROU745@users.noreply.github.com>
2022-10-17 09:22:38 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,217 @@
+<?php include("header.php"); ?>
+
+
+
+
+<div class="row clearmargin clearpadding row-image-txt" style="justify-content: center;background-color: gray; margin:unset;">
+    <div class="col-xs-12 col-sm-6 col-md-6 col-sm-pull-6" style="padding:20px;">
+        <h1>Heading</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel lorem a lectus finibus bibendum ut sagittis diam. Nunc egestas nulla in elit congue, id pellentesque ex semper. Aenean euismod tellus tincidunt massa eleifend, a efficitur lectus ultricies. Aliquam bibendum et leo quis consectetur. Integer consequat consequat mauris, id porttitor sem feugiat eu. Proin ac neque eu nisi viverra mattis. Curabitur molestie lectus ac sollicitudin faucibus. Duis congue ipsum eget justo egestas blandit.</p>
+        <div style="text-align:center"><button class="btn btn-light btn-lg" type="button">Lorem </button></div>
+    </div>
+</div>
+
+<section id="section-timeline">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">About </h2>
+                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur. </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-group timeline">
+                    <li class="list-group-item">
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets3/img3/1.jpg">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>2009-2011 </h4>
+                                <h4 class="subheading">Our Humble Beginnings </h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+                                    ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam,
+                                    recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item timeline-inverted">
+                        <div class="timeline-image">
+                            <img class="rounded-circle img-fluid" src="assets3/img3/2.jpg">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>March 2011 </h4>
+                                <h4 class="subheading">An Agency is Born </h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+                                    ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam,
+                                    recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="timeline-image">
+                            <img class="rounded-circle img-fluid" src="assets3/img3/3.jpg">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>December 2012 </h4>
+                                <h4 class="subheading">Transition to Full Service </h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+                                    ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam,
+                                    recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item timeline-inverted">
+                        <div class="timeline-image">
+                            <img class="rounded-circle img-fluid" src="assets3/img3/4.jpg">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>July 2014 </h4>
+                                <h4 class="subheading">Phase Two Expansion </h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+                                    ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam,
+                                    recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item timeline-inverted">
+                        <div class="timeline-image">
+                            <h4>Be Part Of Our Story!</h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div style="margin:10px 12px 0px 0px ;">
+    <div class="row" data-masonry="{&quot;percentPosition&quot;: true }">
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card">
+                <picture type="" srcset=""><img class="card-img-top p-3" src="assets4/img4/p-1.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture>
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card">
+                <picture type="" srcset=""><img class="card-img-top p-3" src="assets4/img4/p-2.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture>
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card"><img class="card-img-top p-3" src="assets4/img4/p-3.jpg" alt="" style="border-radius: 24px; object-fit: cover">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card">
+                <picture type="" srcset=""><img class="card-img-top p-3" src="assets4/img4/p-4.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture>
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card">
+                <picture type="" srcset=""><img class="card-img-top p-3" src="assets4/img4/p-5.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture>
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card"><img class="card-img-top p-3" src="assets4/img4/p-6.jpg" alt="" style="border-radius: 24px; object-fit: cover">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card">
+                <picture type="" srcset=""><img class="card-img-top p-3" src="assets4/img4/p-7.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture>
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card"><img class="card-img-top p-3" src="assets4/img4/p-8.jpg" alt="" style="border-radius: 24px; object-fit: cover">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo<br></h5>
+                    <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script async="" src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"></script>
+</div>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-auto col-sm-12 col-md-12 col-lg-4 col-xl-4" style="padding-top: 15px;padding-bottom: 15px;padding-right: 15px;padding-left: 15px;">
+                <div class="bg-light border rounded shadow card" data-bss-hover-animate="pulse"><img class="card-img-top" src="assets5/img5/alleStudio2.jpg">
+                    <div class="card-body">
+                        <h3 class="card-title" style="font-family: Antic, sans-serif;color: rgb(81,87,94);">Lorem Ipsum</h3>
+                        <h5 class="card-sub-title" style="font-family: Antic, sans-serif;color: #38ae53;">Webbdesign</h5>
+                        <p class="card-text" style="font-family: Antic, sans-serif;color: rgb(81,87,94);"></p>
+                        <p id="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean. Dignissim cras tincidunt.</p><button class="btn btn-outline-success" type="button" style="font-weight: normal;font-family: Antic, sans-serif;">Lorem in LinkedIn&nbsp;<i class="icon-social-linkedin"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto col-sm-12 col-md-12 col-lg-4 col-xl-4" style="padding-top: 15px;padding-bottom: 15px;padding-right: 15px;padding-left: 15px;margin-right: 0px;margin-bottom: 0px;">
+                <div class="bg-light border rounded shadow card" data-bss-hover-animate="pulse"><img class="card-img-top" src="assets5/img5/alleStudio.jpg">
+                    <div class="card-body">
+                        <h3 class="card-title" style="font-family: Antic, sans-serif;color: rgb(81,87,94);">Lorem Ipsum</h3>
+                        <h5 class="card-sub-title" style="font-family: Antic, sans-serif;color: #38ae53;">Webdesign</h5>
+                        <p class="card-text" style="font-family: Antic, sans-serif;color: rgb(81,87,94);"></p>
+                        <p id="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean. Dignissim cras tincidunt.</p><button class="btn btn-outline-success" type="button" style="font-weight: normal;font-family: Antic, sans-serif;">Lorem på LinkedIn&nbsp;<i class="icon-social-linkedin"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto col-sm-12 col-md-12 col-lg-4 col-xl-4" style="padding-top: 15px;padding-bottom: 15px;padding-right: 15px;padding-left: 15px;">
+                <div class="bg-light border rounded shadow card" data-bss-hover-animate="pulse"><img class="card-img-top" src="assets5/img5/allestudio3.jpg">
+                    <div class="card-body">
+                        <h3 class="card-title" style="font-family: Antic, sans-serif;color: rgb(81,87,94);">Lorem Ipsum</h3>
+                        <h5 class="card-sub-title" style="font-family: Antic, sans-serif;color: #38ae53;">Webdesign</h5>
+                        <p class="card-text" style="font-family: Antic, sans-serif;color: rgb(81,87,94);"></p>
+                        <p id="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean. Dignissim cras tincidunt.</p><button class="btn btn-outline-success" type="button" style="font-weight: normal;font-family: Antic, sans-serif;">Lorem på LinkedIn&nbsp;<i class="icon-social-linkedin"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include("footer.php"); ?>
